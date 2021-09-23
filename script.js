dce0167555086e24069a1a5e9d2be0f2958d63f1
Muhammad Zeeshan Ashraf <zeeshanashraf633b@gmail.com>
2021-09-24 00:04:13 +0500

    Complete Landing Page

--- script.js
+++ script.js
@@ -1,20 +1,76 @@
+// Mouse Cricle
 const mouseCircle = document.querySelector(".mouse-circle");
 const mouseDot = document.querySelector(".mouse-dot");
 
-// Mouse Cricle
 const mouseCircleFn = (x, y) => {
   mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity:1`;
   mouseDot.style.cssText = `top: ${y}px; left: ${x}px; opacity:1`;
 };
 
+//End Of Mouse Cricle
+
+// Animated Cricles
+const circles = document.querySelectorAll(".circle");
+const mainImg = document.querySelector(".main-circle img");
+
+let mX = 0;
+let mY = 0;
+const animationDistance = 100;
+const animateCircles = (e, x, y) => {
+  if (x < mX) {
+    circles.forEach((circle) => {
+      circle.style.left = `${animationDistance}px`;
+    });
+    mainImg.style.left = `${animationDistance}px`;
+  } else if (x > mX) {
+    circles.forEach((circle) => {
+      circle.style.left = `-${animationDistance}px`;
+    });
+    mainImg.style.left = `-${animationDistance}px`;
+  }
+  if (y < mY) {
+    circles.forEach((circle) => {
+      circle.style.top = `${animationDistance}px`;
+    });
+    mainImg.style.top = `${animationDistance}px`;
+  } else if (y > mY) {
+    circles.forEach((circle) => {
+      circle.style.top = `-${animationDistance}px`;
+    });
+    mainImg.style.top = `-${animationDistance}px`;
+  }
+  mX = e.clientX;
+  mY = e.clientY;
+};
+// End of Animated Cricles
+
 document.body.addEventListener("mousemove", (e) => {
   let x = e.clientX;
   let y = e.clientY;
   mouseCircleFn(x, y);
+  animateCircles(e, x, y);
 });
 
 document.body.addEventListener("mouseleave", () => {
   mouseCircle.style.opacity = "0";
   mouseDot.style.opacity = "0";
 });
-//End Of Mouse Cricle
+
+//Main  Button
+const mainBtn = document.querySelector(".main-btn");
+
+let ripple;
+mainBtn.addEventListener("mouseenter", (e) => {
+  const left = e.clientX - e.target.getBoundingClientRect().left;
+  const top = e.clientY - e.target.getBoundingClientRect().top;
+  ripple = document.createElement("div");
+  ripple.classList.add("ripple");
+  ripple.style.left = `${left}px`;
+  ripple.style.top = `${top}px`;
+  mainBtn.prepend(ripple);
+});
+
+mainBtn.addEventListener("mouseleave", () => {
+  mainBtn.removeChild(ripple);
+});
+// End Of Main Button
